23dd75c9802450484ab0291776a7c1a4f3c73ec2
keerthivasa03 <keerthivasa2005@gmail.com>
2026-04-04 19:55:50 +0530

    fix: async-storage version 3

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { ActivityIndicator, View } from "react-native";
+import { useFonts } from "expo-font";
+import {
+  Ionicons,
+  Feather,
+  MaterialIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
+
 import Navigation from "./apps/Navigation/navigation";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    ...Ionicons.font,
+    ...Feather.font,
+    ...MaterialIcons.font,
+    ...SimpleLineIcons.font,
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return <Navigation />;
 }
