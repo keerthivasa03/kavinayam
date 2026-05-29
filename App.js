9201db559bf03f89aba650a0215ea5e411fdaada
keerthivasa03 <keerthivasa2005@gmail.com>
2026-05-29 13:16:29 +0530

    SDK 54 working build

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-//import React from "react";
-//import Navigation from "./apps/Navigation/navigation";
-
-//export default function App() {
- // return <Navigation />;
-//}
-
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import Navigation from "./apps/Navigation/navigation";
 
 export default function App() {
-  return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text>Kavinayam Test</Text>
-    </View>
-  );
+  return <Navigation />;
 }
+
